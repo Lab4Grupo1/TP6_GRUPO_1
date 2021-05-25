6a99e01cdaffd289ea02f126c26e110738af889e
ppnt76 <ppnt_76@hotmail.com>
2021-05-25 12:24:50 -0300

    daolmpl,conexion,entidad, cals persona

---
+++ src/entidad/Persona.java
@@ -0,0 +1,40 @@
+package entidad;
+
+public class Persona {
+	//**Propiedades**//
+	private String dni;
+	private String	nombre;
+	private String apellido;
+	private boolean estado;
+	
+	//****Metodos***//
+	
+	//Get y Set
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public boolean isEstado() {
+		return estado;
+	}
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+	
+	
+
+}
